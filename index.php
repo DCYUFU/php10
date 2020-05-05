6c1f6d874cd23b91e509384879ceb1e1c346e728
DCYUFU <860180@qq.com>
2020-05-05 23:48:29 +0800

    20200505

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>  
+  <head>
+    <title>留言板-20200505</title>
+    <meta charset='utf-8'/>
+  </head>
+<body>
+    <div>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <input type="text">
+      <input type="submit" value='提交'>
+    </div>
+    <div>
+      <div>
+        <p>用户名</p>
+        <p>留言内容</p>
+      </div>
+      <div>
+        <p>用户名</p>
+        <p>留言内容</p>
+      </div>
+      <div>
+        <p>用户名</p>
+        <p>留言内容</p>
+      </div>
+    </div>
+  </body>
+</html>
